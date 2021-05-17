5c8383d3aad51bf911f8d6fbf814ff391cc94a39
Militao36 <matheusmoreira3693@gmail.com>
2021-05-17 10:23:20 -0300

    Commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const server = express()
+
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'ejs');
+
+server.use(express.json())
+
+server.get('/version', (req, res) => {
+    return res.status(200).json({
+        version: '1.0.0'
+    })
+})
+
+server.listen(3000, () => console.log('http://localhost:3000'))
